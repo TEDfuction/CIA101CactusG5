2c479eb57cc5d76db989d161e56e0e460eca4418
LU MING DE <qa58899@gmail.com>
2024-04-01 15:33:05 +0800

    first commit

--- src/main/java/com/cactus/derek/Member.java
+++ src/main/java/com/cactus/derek/Member.java
@@ -1,5 +1,9 @@
 package com.cactus.derek;
 
 public class Member {
-
+	private Integer memNo;
+	private String memName;
+	public static void main(String[] args) {
+		System.out.println("mymyfriend");
+	}
 }
